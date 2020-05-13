9a105d459d904c75b5f4a238b79ae507b950ee02
LuVe96 <LukiVe16@gmail.com>
2020-05-13 15:44:46 +0200

    added first test scene and wallmovement script

--- Assets/Object.cs
+++ Assets/Object.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    RigidBody obj;
+    //RigidBody obj;
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/WallMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Rigidbody rigidbody = GetComponent<Rigidbody>();
+        ////rigidbody.velocity = new Vector3(0, 0, -0.1f);
+        //rigidbody.AddForce(new Vector3(0, 0, -0.2f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, 0, -0.2f * Time.deltaTime);
+      
+    }
+}
