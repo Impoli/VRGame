aaf3a79535514990becf27f3b2147521c18c9565
LuVe96 <LukiVe16@gmail.com>
2020-06-01 17:04:37 +0200

    added torch light

---
+++ Assets/TorchLight.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchLight : MonoBehaviour
+{
+    private Light torch;
+    private float timeSum = 0;
+    private float duration = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        torch = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSum += Time.deltaTime;
+
+        if (timeSum >= duration)
+        {
+            torch.intensity = Random.Range(0.7f, 1.3f);
+            duration = Random.Range(0.01f, 0.3f);
+            timeSum = 0;
+        }
+        
+
+    }
+}
