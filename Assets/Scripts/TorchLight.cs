772fa3b6d9605bdbbdb95832230c80eb208aa848
LuVe96 <LukiVe16@gmail.com>
2020-07-14 21:50:54 +0200

    added torchlight to walls

--- Assets/Scripts/TorchLight.cs
+++ Assets/Scripts/TorchLight.cs
@@ -8,6 +8,8 @@ public class TorchLight : MonoBehaviour
     private float timeSum = 0;
     private float duration = 0.2f;
 
+    public LightingMode lightingMode = LightingMode.Tunnel;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,11 +23,49 @@ public class TorchLight : MonoBehaviour
 
         if (timeSum >= duration)
         {
-            torch.intensity = Random.Range(0.1f, 0.5f);
+            torch.intensity = Random.Range(lightingMode.GetIntense()[0], lightingMode.GetIntense()[1]);
             duration = Random.Range(0.01f, 0.3f);
             timeSum = 0;
         }
         
+    }
+}
 
+public enum LightingMode
+{
+   Wall,
+   Tunnel
+}
+
+
+// extension für enum: PlayerSpeed
+static class PlayerSpeedMethods
+{
+
+    public static float [] GetIntense(this LightingMode p1)
+    {
+        switch (p1)
+        {
+            case LightingMode.Wall:
+                return new float[] { 0.1f, 0.3f };
+            case LightingMode.Tunnel:
+                return new float[] { 0.01f, 0.3f };
+            default:
+                return new float[] {0f, 0f };
+
+        }
+    }
+
+    public static float GetEndVal(this LightingMode p1)
+    {
+        switch (p1)
+        {
+            case LightingMode.Wall:
+                return 0.25f;
+            case LightingMode.Tunnel:
+                return 0.3f;
+            default: return 0;
+
+        }
     }
 }
